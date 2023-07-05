96a1a6636604f13c7fb56e153a0cce92c46c8a41
Evan <ev.artby@gmail.com>
2023-07-05 11:42:05 +1000

    made back button, menu button and title text

--- lib/play_page.dart
+++ lib/play_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:spotifyapp/neu_box.dart';
 
 class PlayPage extends StatefulWidget {
   const PlayPage({Key? key}) : super(key: key);
@@ -11,12 +14,41 @@ class _PlayPageState extends State<PlayPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Container(
-        height: 100,
-        width: 100,
-        decoration: BoxDecoration(color: Colors.grey[300]),
-      ),
-    ));
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              children: [
+                //back button and menu
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      height: 60,
+                      width: 60,
+                      child:
+                          NeuBox(child: Icon(Icons.arrow_back_ios_new_rounded)),
+                    ),
+                    Text('S P O T I F Y'),
+                    SizedBox(
+                      height: 60,
+                      width: 60,
+                      child: NeuBox(child: Icon(Icons.menu_rounded)),
+                    ),
+                  ],
+                )
+
+                //cover art, artist and song name
+
+                //start time, shuffle, repeat , end time
+
+                //linear bar
+
+                //prev song, pause and play, next song
+              ],
+            ),
+          ),
+        ));
   }
 }
